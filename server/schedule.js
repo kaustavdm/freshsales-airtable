cbefcb341428324966149e65b3ff177947403f24
Kaustav Das Modak <kaustav.dasmodak@gmail.com>
2020-03-02 16:20:16 +0530

    Extract schedule logic into separate file
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@gmail.com>

---
+++ server/schedule.js
@@ -0,0 +1,27 @@
+/* global $schedule */
+
+module.exports = {
+  name: 'freshsales-airtable-sync',
+
+  create (payload) {
+    var opts = {
+      name: this.name,
+      data: payload,
+      schedule_at: new Date((new Date()).getTime() + 60000).toISOString(), // 1 mins from now
+      repeat: {
+        time_unit: 'minutes',
+        frequency: 1
+      }
+    }
+    return $schedule.create(opts)
+      .fail(function (err) {
+        console.error('Cannot schedule sync\n', err.message)
+      })
+  },
+
+  delete () {
+    return $schedule.delete({
+      name: this.name
+    })
+  }
+}
